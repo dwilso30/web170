2b159c2143f0f1c4786ab741657a0fd15d21570f
Danielle Wilson <dwilso30@seattlecentral.edu>
2017-05-05 09:56:22 -0700

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Theme Name: Web170 
+Theme URI:https://github.com/dwilso30/web170
+Author: Danielle Wilson
+Author URI:www.wairatawilson.com
+Description:This is the theme for my web170 class project. Spring 2017
+Version:1
+*/
+
+
+?>
