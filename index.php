15d2f4a1725856dc3c370ca65fe9b123249b428d
Danielle Wilson <dwilso30@seattlecentral.edu>
2017-05-26 23:34:41 -0700

    custom posts feed w/featured image

--- index.php
+++ index.php
@@ -7,12 +7,13 @@
 
 <?php 
 if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<article class="post-excerpt">
 <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
 <small>Posted on <?php the_time( 'F j, Y'); ?> by <?php the_author(); ?> <?php the_category();?> </small>
 	<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>	
 		<p><?php echo get_the_excerpt(); ?><a href="<?php the_permalink(); ?>">Read More&nbsp;&raquo;</a></p>
-	
+	</article>
 <?php endwhile; endif; ?>
 <small>index.php</small>
 
